98a7499389a8acd173821fe531312eedafb38346
ranalytica <ranalyticas@gmail.com>
2020-05-14 23:02:32 -0500

    overly over

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,18 +7,28 @@ function guess(){
     while true;
     do
         echo "Enter guess here: "
+
         read  number
+
         if [ $number -lt $the_truth ]
         then
-            echo "your guess is Less then the true number"
+
+            echo "Your guess is Less then the true number, try again: "
+
         elif [ $number -gt $the_truth ]
         then
-            echo "your guess is Greater then the true number"
+
+            echo "Your guess is greater than the true number, try again: "
         else
-            echo "Cheers, you are right!"
+
+            echo "Cheers, you are right padawan.  The force is strong with this one!"
+
         break;
+
         fi
+
     done
+
 }
-echo "Guess how many files you have in current directory!"
+echo "Guess how many files you have in current directory, padawan!"
 guess
